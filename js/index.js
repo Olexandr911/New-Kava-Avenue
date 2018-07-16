5db6d04ad608854ac045ae87311050649ba4e8b9
Olexandr <olexandr911@gmail.com>
2018-07-16 12:31:20 +0000

    add new file

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
-    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
-	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
-        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
-        }
-	    return false; // выключаем стандартное действие
+    $(".menu-main").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 500);
     });
 });
